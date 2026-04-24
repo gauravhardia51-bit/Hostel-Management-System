7a8bc3544874a01c88e1a59cef89167b657058c7
New Name <@ankitaha00>
2026-04-24 18:45:28 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-//import axios from 'axios';
-import { Routes, Route } from 'react-router';
-// //import { useState } from 'react';
-import { HomePage } from './pages/HomePage';
-// import { CheckoutPage } from './pages/checkout/CheckoutPage';
-// import { OrdersPage } from './pages/orders/OrdersPage';
-import './App.css'
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  // const [cart, setCart] = useState([]);
-
-  // const loadCart = async () => {
-  //   const response = await axios.get('/api/cart-items?expand=product');
-  //   setCart(response.data);
-  // };
-
-  //useEffect(() => { loadCart(); }, []);
-
   return (
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path="rooms" element={<div>Rooms Available</div>}/>
-      <Route path="rooms" element={<div>Only for Students</div>}/>
-    </Routes>
-  )
+    <div>
+      <Dashboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
